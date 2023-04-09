7cc8082a06400d08dc7d474e05c2a6cfe5058a41
Sergey Mikshun <sininto@mail.ru>
2023-04-09 16:18:29 +0300

    new_rep

--- handlers/default_heandlers/start.py
+++ handlers/default_heandlers/start.py
@@ -5,7 +5,7 @@ from loader import bot
 @bot.message_handler(commands=['start'])
 def bot_start(message: Message):
     bot.reply_to(message, f"Привет,\n"
-                              f"Я помогу вам подобрать лучшие рестораны Милана\n"
+                              f"Я помогу вам подобрать лучшие рестораны\n"
                               f"Команды вы можете увидеть благодаря функции /help или слева от поля ввода в кнопочке меню)")
 
 
